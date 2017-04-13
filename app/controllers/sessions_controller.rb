c3fcc838f90ee63871b8283bad50ec8e3a18a94f
masaaki1915 <masaaki1915@gmail.com>
2017-04-13 21:13:12 +0900

    sessions#create,destroy定義

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
-  def new
+  def new ; end
+
+  def create
+    user = User.find_by(login_params[:email].downcase)
+    if user && user.authenticate(login_params[:password])
+    else
+      flash.now[:danger] = "invalid email/password combination"
+      render 'new'
+    end
   end
+
+  def destroy ; end
+
+  private
+    def login_params
+      params.require(:session).permit(:email, :password)
+    end
 end
