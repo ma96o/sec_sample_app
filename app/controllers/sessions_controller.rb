ebdaf670237c8c3341b953a36ff2683a15b79570
masaaki1915 <masaaki1915@gmail.com>
2017-04-14 17:04:41 +0900

    remember_meに対するテスト

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: login_params[:email].downcase)
     if user && user.authenticate(login_params[:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = "invalid email/password combination"
