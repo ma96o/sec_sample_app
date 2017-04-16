55cf349d75e2042efff5771ea807ff89deacd351
masaaki1915 <masaaki1915@gmail.com>
2017-04-16 22:32:30 +0900

    有効化にともないログイン方法変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,18 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: login_params[:email].downcase)
     if @user && @user.authenticate(login_params[:password])
-      log_in @user
-      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = 'account not activated'
+        message += 'check your email for the activation link'
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      flash.now[:danger] = "invalid email/password combination"
+      flash.now[:danger] = "Invalid email/password combination"
       render 'new'
     end
   end
