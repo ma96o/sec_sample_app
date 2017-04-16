0ce560813ecbac998a88cde148b71685562ce915
masaaki1915 <masaaki1915@gmail.com>
2017-04-16 22:12:10 +0900

    users#createアクション修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.account_activation(@user).deliver_now
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      flash[:notice] = "Please check your email to activate your account"
+      redirect_to root_url
     else
       render 'new'
     end
