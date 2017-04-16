ff775d2670300cb60c80c6f665313a8e15e177b4
masaaki1915 <masaaki1915@gmail.com>
2017-04-16 23:29:03 +0900

    アカウント有効化メソッド定義

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,20 @@ class SessionsController < ApplicationController
   def new ; end
 
   def create
-    @user = User.find_by(email: login_params[:email].downcase)
-    if @user && @user.authenticate(login_params[:password])
-      if user.activated?
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
       else
-        message = 'account not activated'
-        message += 'check your email for the activation link'
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
         flash[:warning] = message
         redirect_to root_url
       end
     else
-      flash.now[:danger] = "Invalid email/password combination"
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
-      log_in @user
+      @user.send_activation_email
       flash[:notice] = "Please check your email to activate your account"
       redirect_to root_url
     else
